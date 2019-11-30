7ae2b40da090343261d53e5309b11396a75dd90e
Dmitriy McCoy <mccoy1@yandex.ru>
2019-11-30 18:21:24 +0300

    Updates

---
+++ precache-manifest.5681f422686c356389bc8a25a302829c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a58d1f65195f2662e056e4e4ba17a1",
+    "url": "/socialspa/index.html"
+  },
+  {
+    "revision": "8e368d831aa424f1c1cf",
+    "url": "/socialspa/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bdb16a49541a641217a8",
+    "url": "/socialspa/static/css/3.1aff8e8a.chunk.css"
+  },
+  {
+    "revision": "df6f375d60dee7eec836",
+    "url": "/socialspa/static/css/4.fdf48a17.chunk.css"
+  },
+  {
+    "revision": "9f27cdc65c53c96679c5",
+    "url": "/socialspa/static/css/main.da4ac400.chunk.css"
+  },
+  {
+    "revision": "8e368d831aa424f1c1cf",
+    "url": "/socialspa/static/js/2.9505b2c7.chunk.js"
+  },
+  {
+    "revision": "bdb16a49541a641217a8",
+    "url": "/socialspa/static/js/3.350ac7dc.chunk.js"
+  },
+  {
+    "revision": "df6f375d60dee7eec836",
+    "url": "/socialspa/static/js/4.38acc381.chunk.js"
+  },
+  {
+    "revision": "9f27cdc65c53c96679c5",
+    "url": "/socialspa/static/js/main.fc3f43c3.chunk.js"
+  },
+  {
+    "revision": "d7c33dec5e366d012434",
+    "url": "/socialspa/static/js/runtime-main.848d583d.js"
+  },
+  {
+    "revision": "df277dad24c28553c65f69075e8ac291",
+    "url": "/socialspa/static/media/preloader.df277dad.gif"
+  }
+]);
